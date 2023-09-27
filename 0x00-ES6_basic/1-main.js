f3f81788abf08405ca6903ea5d099c386155edcb
6godpro <erickson5115@gmail.com>
2023-09-27 01:56:16 +0100

    initial upload task 1

--- 0x00-ES6_basic/1-main.js
+++ 0x00-ES6_basic/1-main.js
@@ -1,11 +1,3 @@
-import createEmployeesObject from './11-createEmployeesObject.js';
-import createReportObject from './12-createReportObject.js';
+import getBudgetForCurrentYear from './8-getBudgetCurrentYear.js';
 
-const employees = {
-    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
-    ...createEmployeesObject('marketing', ['Sylvie'])
-};      
-
-const report = createReportObject(employees);
-console.log(report.allEmployees);
-console.log(report.getNumberOfDepartments(report.allEmployees));
+console.log(getBudgetForCurrentYear(2100, 5200, 1090));
