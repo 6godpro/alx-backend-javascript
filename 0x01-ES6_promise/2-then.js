96b51a8cfd8a7765c33bfad5e105fa245e885a9d
6godpro <erickson5115@gmail.com>
2023-09-27 02:20:52 +0100

    included a finally clause

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,6 @@
 module.exports = function handleResponseFromAPI(promise) {
   promise
-    .then((success) => {
-      console.log('Got a response from the API');
-      return { status: 200, body: success };
-    })
-    .catch((error) => { // eslint-disable-line no-unused-vars
-      console.log('Got a response from the API');
-      return Error();
-    });
+    .then((success) => ({ status: 200, body: success }))
+    .catch(() => Error())
+    .finally(console.log('Got a response from the API'));
 };
