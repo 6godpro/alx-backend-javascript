a1fd8aabf4c1be8a3cd8feaff2f99885c05a754d
6godpro <erickson5115@gmail.com>
2023-09-27 02:57:02 +0100

    initial commit task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise]).then((results) => {
+    const res = [];
+
+    results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+            res.push({ status: result.status, value: result.value });
+        } else if (result.status === 'rejected') {
+            res.push({ status: result.status, value: result.reason })
+        }
+    });
+    return res;
+  });
+}
