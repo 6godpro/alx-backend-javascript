33229f2e5b45d105e2c147ebea687772afa148df
6godpro <erickson5115@gmail.com>
2023-09-28 02:45:48 +0100

    reverted code

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,10 +1,15 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-  try {
-    return { photo, user };
-  } catch {
-    return { photo: null, user: null };
-  }
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((res) => {
+      const result = {};
+      const [photo, user] = res;
+
+      result.photo = photo;
+      result.user = user;
+
+      return result;
+    })
+    .catch(() => ({ photo: null, user: null }));
 }
