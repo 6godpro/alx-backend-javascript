68a945cd4ccd1c44dfb0907d2bc31e1ad3fbe011
6godpro <erickson5115@gmail.com>
2023-09-28 02:43:57 +0100

    condensed code using async/await syntax

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,23 +1,10 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  // return Promise.all([uploadPhoto(), createUser()])
-  //   .then((res) => {
-  //     const result = {};
-  //     const [photo, user] = res;
-
-  //     result.photo = photo;
-  //     result.user = user;
-
-  //     return result;
-  //   })
-  //   .catch(() => {
-  //     return { photo: null, user: null }
-  //   });
   const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
   try {
-    return {photo, user}
+    return { photo, user };
   } catch {
-    return { photo: null, user: null }
+    return { photo: null, user: null };
   }
 }
