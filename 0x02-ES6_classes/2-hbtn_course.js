11428aa1d94b7dbe5c19268e6c241654713bbf00
6godpro <erickson5115@gmail.com>
2023-10-01 01:33:40 +0100

    initial commit

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,46 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== typeof String()) {
+      throw TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== typeof Number()) {
+      throw TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value)) {
+      throw TypeError('Students must be an array of strings');
+    } else {
+      value.forEach((element) => {
+        if (typeof element !== typeof String()) {
+          throw TypeError('Students must be an array of strings');
+        }
+      });
+    }
+    this._students = value;
+  }
+}
