0092b064ceccab35f6a3b6b572247a877ba1b3a9
Eric Eromosele <109639426+6godpro@users.noreply.github.com>
2023-10-03 02:54:24 +0100

    remove useless whitespace

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,6 +2,5 @@ export default function getStudentsByLocation(listStudents, city) {
   if (!Array.isArray(listStudents)) {
     return [];
   }
-
   return listStudents.filter((student) => student.location === city);
 }
