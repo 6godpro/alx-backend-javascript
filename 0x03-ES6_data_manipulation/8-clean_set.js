704cb98100a282e0664ed9ff8eec4779bd199d77
6godpro <erickson5115@gmail.com>
2023-10-04 02:43:04 +0100

    added extra check for potential undefined strings contained within set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,12 +2,11 @@ export default function cleanSet(set, startString) {
   if (startString === undefined || startString.length === 0) {
     return '';
   }
-  // const result = [];
-  // set.forEach((element) => {
-  //   if (element.startsWith(startString)) {
-  //     result.push(element.slice(startString.length));
-  //   }
-  // });
-  // return result.join('-');
-  console.log([...set])
+  const result = [];
+  set.forEach((element) => {
+    if (element !== undefined && element.startsWith(startString)) {
+      result.push(element.slice(startString.length));
+    }
+  });
+  return result.join('-');
 }
