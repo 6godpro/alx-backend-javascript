460de53527e434b42262047ae9e78e6f47067a1b
6godpro <erickson5115@gmail.com>
2023-10-04 02:32:31 +0100

    added extra check for invalid argument

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,11 +2,12 @@ export default function cleanSet(set, startString) {
   if (startString === undefined || startString.length === 0) {
     return '';
   }
-  const result = [];
-  set.forEach((element) => {
-    if (element.startsWith(startString)) {
-      result.push(element.slice(startString.length));
-    }
-  });
-  return result.join('-');
+  // const result = [];
+  // set.forEach((element) => {
+  //   if (element.startsWith(startString)) {
+  //     result.push(element.slice(startString.length));
+  //   }
+  // });
+  // return result.join('-');
+  console.log([...set])
 }
