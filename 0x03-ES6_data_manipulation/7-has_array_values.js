5d7731489102f4fd06069fc143086f7a38cf45b7
6godpro <erickson5115@gmail.com>
2023-10-04 02:18:19 +0100

    initial commit

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,6 @@
 export default function hasValuesFromArray(set, array) {
-  for (const element in array) {
-    if (!set.has(element)) {
+  for (const element of array) {
+    if (!(set.has(element))) {
       return false;
     }
   }
